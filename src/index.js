17dfa057c5bca171ddffbabdd4ab33020bc02ba8
jinxin <123896618@qq.com>
2018-09-14 09:07:08 +0800

    loader

--- src/index.js
+++ src/index.js
@@ -1,4 +1,22 @@
-document.getElementById('root').innerHTML = 'hello king123';
+import './my.less';
+import pic from './pic.jpg';
+let root = document.getElementById('root');
+let img = new Image();
+img.src = pic;
+root.appendChild(img);
+
+function handsome(target) {
+    target.prototype.face = 'handsome'
+}
+
+@handsome
+class Person {
+
+}
+
+let king = new Person();
+console.log(king.face);
+
 
 if(module.hot){
     module.hot.accept()
--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,65 @@ const path = require('path');
 const webpack = require('webpack');
 const htmlWebpackPlugin = require('html-webpack-plugin');
 const cleanWebpackPlugin = require('clean-webpack-plugin');
+const miniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
     mode: 'development',
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: {
+        'index': './src/index.js'
+    },
     output:{
         filename: '[name].[hash:8].js',
         path: path.resolve(__dirname, 'dist')
     },
     devServer: {
         contentBase: './dist',
-        port: 3000,
+        port: 3333,
         hot: true
     },
-    module:{},
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: [{
+                    loader: miniCssExtractPlugin.loader
+                }, 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [{
+                    loader: miniCssExtractPlugin.loader
+                }, 'css-loader', 'less-loader']
+            },
+            {
+                test: /[.png|.jpg]$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8*1024
+                    }
+                }
+            },
+            {
+                test: /.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /.html$/,
+                use: 'html-withimg-loader'
+            }
+        ]
+    },
     plugins:[
+        new miniCssExtractPlugin({
+            filename: 'css/style.css'
+        }),
         new webpack.HotModuleReplacementPlugin(),
         new cleanWebpackPlugin(['./dist']),
         new htmlWebpackPlugin({
             template: './src/index.html',
             filename: 'index.html',
+            chunks: ['index']
         })
     ],
     resolve:{}
